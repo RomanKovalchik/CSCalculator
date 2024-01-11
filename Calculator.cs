bdbfe93aaeb13cdfba0e5cd09b1a7d23fa0e22f0
RomanKovalchik <kovalchykrv@yandex.ru>
2024-01-11 16:43:25 +0300

    Добавьте файлы проекта.

---
+++ Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal static class Calculator
+    {
+
+        
+        private static double result;
+
+        public static double calculate(double x, double y, char operation)
+        {
+
+            switch (operation)
+            {
+                case '+':
+                    result = x + y;  
+                    break;
+                case '-':
+                    result = x - y;
+                    break;
+                case '*':
+                    result = x * y;
+                    break;
+                case '/':
+                    result = x / y;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+
+            return result;
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculator;
+
+double val1 = 0;
+double val2 = 0;
+char opcode = ' ';
+
+string input = "";
+
+do {
+
+    
+    
+    Console.WriteLine("Enter number 1");
+    val1  = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter number 2");
+    val2 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter action, like '+', '-', '*', '/'");
+    opcode = Convert.ToChar(Console.ReadLine());
+    double val = Calculator.Calculator.calculate(val1, val2, opcode);
+    
+    Console.WriteLine(val);
+    Console.WriteLine("Would you like to continue? If yes - press 'y' key");
+    input = Console.ReadLine();
+
+} while (input == "Y"|| input=="y");
+
+
